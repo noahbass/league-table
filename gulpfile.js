52e5a31380145e4a93f02eab0336afd1181ea60a
Noah Bass <bassjnoah@gmail.com>
2015-06-07 14:57:59 -0400

    Updated gulpfile.js with js, css, nodemon tasks
    - new js, css, nodemon tasks
    - new default and build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,88 @@
-var gulp       = require('gulp'),
-    gulpConcat = require('gulp-concat'),
-    gulpBower  = require('gulp-bower');
+/**
+ * gulpfile.js
+ */
 
-/*
- * Run `bower install`
+var gulp      = require('gulp'),
+    concatJS  = require('gulp-concat'),
+    uglify    = require('gulp-uglify'),
+    bower     = require('gulp-bower'),
+    concatCSS = require('gulp-concat-css'),
+    minify    = require('gulp-minify-css'),
+    nodemon   = require('gulp-nodemon');
+
+var dist = './public/dist';
+
+/**
+ * bower task: run `bower install`
  */
 gulp.task('bower', function() {
-    return gulpBower();
+    return bower();
 });
 
 
-/*
- * Concat and uglify javascript
+/**
+ * js task: concat and uglify javascript
  */
-gulp.task('js', function() {
+gulp.task('js', ['bower'], function() {
     return gulp.src([
-            './bower_components/angular/angular.min.js',
-            './bower_components/angular-route/angular-route.min.js',
-            './public/js/app.js',
-            './public/js/factories/*',
-            './public/js/controllers/*'
-        ])
-        .pipe(gulpConcat('all.js'))
-        .pipe(gulp.dest('./public/dist'));
+        './bower_components/angular/angular.min.js',
+        './bower_components/angular-route/angular-route.min.js',
+        './public/js/app.js',
+        './public/js/factories/*',
+        './public/js/controllers/*'
+    ])
+    .pipe(concatJS('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(dist));
 });
+
+
+/**
+ * css task: concat and minify CSS
+ */
+gulp.task('css', ['bower'], function() {
+    return gulp.src([
+        './bower_components/bootswatch/flatly/bootstrap.css',
+        './public/assets/css/style.css'
+    ])
+    .pipe(concatCSS('all.min.css'))
+    .pipe(minify())
+    .pipe(gulp.dest(dist));
+});
+
+
+/**
+ * nodemon task: run nodemon and watch for asset changes
+ */
+gulp.task('nodemon', function() {
+    nodemon({
+        script: './bin/www'
+    });
+
+    gulp.watch([
+        './public/js/**/*.js',
+        './public/assets/js/**/*.js'
+    ], ['js']);
+    gulp.watch('./public/assets/css/**/*.css', ['css']);
+});
+
+
+/**
+ * default task: build js and css, then run nodemon to watch
+ */
+gulp.task('default', [
+    'bower',
+    'js',
+    'css',
+    'nodemon'
+]);
+
+
+/**
+ * build task: build js and css
+ */
+gulp.task('build', [
+    'bower',
+    'js',
+    'css'
+]);
